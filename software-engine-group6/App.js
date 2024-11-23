a7b0902559520ff21486e541392220e1aa698ae4
rawatrawat <167859881+rawatrawat@users.noreply.github.com>
2024-11-23 15:12:08 +0800

    Update App.js

--- software-engine-group6/App.js
+++ software-engine-group6/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { CartProvider } from './components/CartContext';
 import LoginScreen from './components/LoginScreen.js';
 import MainMenuScreen from './components/MainMenuScreen.js';
+import MenuScreen from './components/MenuScreen.js';
 import RegistrationSelectionScreen from './components/RegistrationSelectionScreen.js';
 import CustomerRegistrationScreen from './components/CustomerRegistrationScreen.js';
 import RestaurantRegistrationScreen from './components/RestaurantRegistrationScreen.js';
@@ -12,15 +14,18 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <CartProvider>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="MainMenu" component={MainMenuScreen} />
+        <Stack.Screen name="Menu" component={MenuScreen} />
         <Stack.Screen name="Register" component={RegistrationSelectionScreen} />
         <Stack.Screen name="RegisterCus" component={CustomerRegistrationScreen} />
         <Stack.Screen name="RegisterRes" component={RestaurantRegistrationScreen} />
         <Stack.Screen name="Order" component={OrderScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+  <CartProvider>
   );
 }
