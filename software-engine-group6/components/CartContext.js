f17b7b778bc8cbdd18a0452f62089b367e0242d7
rawatrawat <167859881+rawatrawat@users.noreply.github.com>
2024-11-24 19:24:07 +0800

    Update CartContext.js

--- software-engine-group6/components/CartContext.js
+++ software-engine-group6/components/CartContext.js
@@ -1,9 +1,27 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const loadCart = async () => {
+      const storedCart = await AsyncStorage.getItem('@cart');
+      if (storedCart) {
+        setCart(JSON.parse(storedCart));
+      }
+    };
+    loadCart();
+  }, []);
+
+  useEffect(() => {
+    const saveCart = async () => {
+      await AsyncStorage.setItem('@cart', JSON.stringify(cart));
+    };
+    saveCart();
+  }, [cart]);
 
   const addToCart = (item) => {
     setCart((prevCart) => [...prevCart, { ...item, quantity: 1 }]);
