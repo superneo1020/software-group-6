6bc4eb0efa386b08e27241a8783e3630d8c72022
rawatrawat <167859881+rawatrawat@users.noreply.github.com>
2024-11-24 19:27:55 +0800

    Update MainMenuScreen.js

--- software-engine-group6/components/MainMenuScreen.js
+++ software-engine-group6/components/MainMenuScreen.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-
 const MainMenuScreen = () => {
   const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Main Menu</Text>
-      <Button title="Logout" onPress={() => navigation.navigate('Login')} />
       <Button title="MENU" onPress={() => navigation.navigate('Menu')} />
+      <Button title="Track" onPress={() => navigation.navigate('OrderTracking')} />
+      <Button title="review" onPress={() => navigation.navigate('Feedback')} />
+      <Button title="history" onPress={() => navigation.navigate('OrderHistory')} />
+      <Button title="report" onPress={() => navigation.navigate('Reporting')} />
+      <Button title="manage" onPress={() => navigation.navigate('RestaurantMenuManagement')} />
+      <Button title="Logout" onPress={() => navigation.navigate('Login')} />
+      
     </View>
   );
 };
@@ -26,6 +31,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 20,
   },
+  
 });
 
 export default MainMenuScreen;
