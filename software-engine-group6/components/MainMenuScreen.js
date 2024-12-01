e515ac833017372d0533dd12e6002e9f2f198c98
rawatrawat <167859881+rawatrawat@users.noreply.github.com>
2024-12-02 02:23:59 +0800

    Update MainMenuScreen.js

--- software-engine-group6/components/MainMenuScreen.js
+++ software-engine-group6/components/MainMenuScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
@@ -8,14 +8,27 @@ const MainMenuScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Main Menu</Text>
-      <Button title="MENU" onPress={() => navigation.navigate('Menu')} />
-      <Button title="Track" onPress={() => navigation.navigate('OrderTracking')} />
-      <Button title="review" onPress={() => navigation.navigate('Feedback')} />
-      <Button title="history" onPress={() => navigation.navigate('OrderHistory')} />
-      <Button title="report" onPress={() => navigation.navigate('Reporting')} />
-      <Button title="manage" onPress={() => navigation.navigate('RestaurantMenuManagement')} />
-      <Button title="Logout" onPress={() => navigation.navigate('Login')} />
-      
+      <View style={styles.buttonContainer}>
+        <Button title="MENU" onPress={() => navigation.navigate('Menu')} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Track" onPress={() => navigation.navigate('OrderTracking')} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="review" onPress={() => navigation.navigate('Feedback')} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="history" onPress={() => navigation.navigate('OrderHistory')} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="report" onPress={() => navigation.navigate('Reporting')} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="manage" onPress={() => navigation.navigate('RestaurantMenuManagement')} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Logout" onPress={() => navigation.navigate('Login')} />
+      </View>
     </View>
   );
 };
@@ -31,7 +44,9 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 20,
   },
-  
+  buttonContainer: {
+    marginVertical: 10, 
+  },
 });
 
 export default MainMenuScreen;
